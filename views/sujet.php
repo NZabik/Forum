9811a8b3b1c0aa1d516b70ecda27af06f5d004fc
Nicolas Zabik <melchiah.nico@gmail.com>
2023-11-22 10:47:05 +0100

    le forum fonctionne

--- views/sujet.php
+++ views/sujet.php
@@ -1,7 +1,7 @@
 <?php
 if (session_status() !== PHP_SESSION_ACTIVE) session_start();
 require_once "../includes/head.php";
-require_once "../controllers/sujetcontroller.php";
+require_once "../controllers/connexiondb.php";
 ?>
 <link rel="stylesheet" href="../CSS/sujet.css">
 <div id="accueil">
@@ -11,7 +11,29 @@ require_once "../controllers/sujetcontroller.php";
     }
     ?>
 </div>
-<!-- <?php echo '<h1 id="register-title">' . $rows['Nom_sujet'] . '</h1>'?> -->
+<?php 
+$con = connectdb();
+date_default_timezone_set('Europe/Paris');
+$req = "SELECT sujet.Id_SUJET,sujet.Nom_sujet FROM sujet where sujet.Id_SUJET='" . $_GET['id'] . "'";
+$req2 = "SELECT discussion.Id_DISCUSSION,discussion.commentaire,discussion.datecommentaire,sujet.Id_SUJET,utilisateur.Nom,utilisateur.Prénom,sujet.Nom_sujet FROM sujet join discussion on discussion.Id_SUJET = sujet.Id_SUJET join utilisateur on utilisateur.Id_UTILISATEUR = discussion.id_UTILISATEUR WHERE sujet.Id_SUJET='" . $_GET['id'] . "' ORDER BY discussion.Id_DISCUSSION";
+$response = $con->query( $req );
+$response2 = $con->query( $req2 );
+$row2= $response->fetch();
+$rows= $response2->fetchAll();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $comm = $_POST['commentaire'];
+    $date3 = new DateTime();
+    if ($comm == "") {
+
+        header("Refresh:0");
+} else {
+    $req = $con->prepare("INSERT INTO discussion (commentaire,datecommentaire,Id_SUJET,id_UTILISATEUR) VALUES (?,?,?,?)");
+		$req->execute(array($comm,$date3->format('d/m/Y à H:i:s'),$_GET['id'],$_SESSION['idUser']));
+        header("Refresh:0");
+}
+}
+echo '<h1 id="register-title">' . $row2['Nom_sujet'] . '</h1>';
+?>
 
 <table class="table table-striped" id="myTable">
     <thead>
@@ -34,9 +56,9 @@ require_once "../controllers/sujetcontroller.php";
         ?>
     </tbody>
 </table>
-<form action="../controllers/sujetcontroller.php" method="post" id="formulaire">
+<form action="" method="post" id="formulaire">
     <div id="ajout" class="form-field">
-        <input type="text" id="commentaire" name="commentaire" placeholder="Ajouter un commentaire">
+        <input type="text" id="sujet" name="commentaire" placeholder="Ajouter un commentaire">
         <input type="submit" id="btn" value="Ajouter">
     </div>
 </form>
